44029539f34fd2e08fefd0f4d7e0aaf451057bb5
Asia Mayfield <a.mayfield18@gmail.com>
2021-03-22 00:53:01 -0700

    working

--- src/Components/IngredientsForm.js
+++ src/Components/IngredientsForm.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as yup from 'yup'
 const schema = yup.object().shape({
-  name: yup.string().required('name is required').min(2, 'name must be at least 2 characters')
+    name: yup.string().required('name is required').min(2, 'name must be at least 2 characters'),
+    size: yup.number()
 })
 export default function IngredientsForm () {
   const [form, setForm] = useState ({name: ''})
@@ -13,6 +14,21 @@ export default function IngredientsForm () {
     .then(() => setErrors({...errors, [name]: ''}))
     .catch(err => setErrors({...errors, [name]: err.errors[0]}))
   }
+
+const submit = event => {
+    event.preventDefault();
+
+    const pizzaOrder = {
+        name: document.getElementById("name").value,
+        size: document.getElementById("size").value
+      };
+  
+      axios.post(`https://reqres.in/api/users`, { pizzaOrder })
+        .then(apiResult => {
+          console.log(apiResult);
+          console.log(apiResult.data);
+        })
+}
 const change = event => {
   const {checked, value, name, type} = event.target
   const valueToUse = type === 'checkbox' ? checked : value
@@ -25,14 +41,16 @@ useEffect(() => {
 return (
 <div className= "IngredientsForm">
    <div> {errors.name}</div>
-   <form>
+   <form onSubmit={submit}>
      <label>Name
-<input onChange={change} value={form.name} name="name" />
+<input onChange={change} value={form.name} name="name" id="name"/>
      </label>
      <label> Sizes
-       <select onChange={change} value={form.Sizes} name="Sizes">
-         <option value="">Select option</option>
+       <select onChange={change} value={form.Sizes} name = "size" id = "size">
+         Select size
          <option value="1">Large</option>
+         <option value="2">Medium</option>
+         <option value="3">Small</option>
          </select>
      </label>
      <button disabled={disabled}>submit</button>
